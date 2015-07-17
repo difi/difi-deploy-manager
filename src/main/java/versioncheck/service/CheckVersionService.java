7bfcd1ee0122bd1c793c68e377d5f158a2d21613
Stein-Bjarne <stein-bjarne.johansen@inmeta.com>
2015-07-17 15:32:13 +0200

    Added check if object for download is already in download list

--- src/main/java/versioncheck/service/CheckVersionService.java
+++ src/main/java/versioncheck/service/CheckVersionService.java
@@ -15,6 +15,8 @@ import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.format;
+
 @Service
 public class CheckVersionService {
     private final Environment environment;
@@ -45,9 +47,12 @@ public class CheckVersionService {
                 JSONObject json = checkVersionDto.retrieveExternalArtifactStatus(url);
                 ApplicationList downloadedApps = checkVersionDto.retrievePreviousDownloadedList();
 
-                if (isDownloaded(json, downloadedApps, json.getString("version"))) {
+                if (isInDownloadList(json, checkVersionDto.retrieveDownloadList())) {
+                    statuses.add(new Status(StatusCode.SUCCESS, format("%s is already in download list", url)));
+                }
+                if (isDownloaded(json, downloadedApps)) {
                     statuses.add(new Status(StatusCode.SUCCESS,
-                            String.format("Latest version of %s is already downloaded.", url)));
+                            format("Latest version of %s is already downloaded.", url)));
                 } else {
                     ApplicationData data = new ApplicationData();
                     data.setName(MonitoringApplications.SPRINGFRAMEWORK_JDBC);
@@ -55,21 +60,21 @@ public class CheckVersionService {
                     applicationsToDownload.add(data);
 
                     statuses.add(new Status(StatusCode.SUCCESS,
-                            String.format("Application %s is set for download.", data.getName())));
+                            format("Application %s is set for download.", data.getName())));
                 }
             } catch (MalformedURLException e) {
                 statuses.add(new Status(StatusCode.ERROR,
-                        String.format("Failed to compose url: %s Reason: %s", url, e.getMessage())));
+                        format("Failed to compose url: %s Reason: %s", url, e.getMessage())));
             } catch (SocketTimeoutException e) {
                 statuses.add(new Status(StatusCode.ERROR,
-                        String.format("Timeout occured. Took too long to read from %s Reason: %s", url, e.getMessage())));
+                        format("Timeout occured. Took too long to read from %s Reason: %s", url, e.getMessage())));
             }
             catch (IOException | ConnectionFailedException e) {
                 statuses.add(new Status(StatusCode.ERROR,
-                        String.format("Failed to retrieve data from %s Reason: %s", url, e.getMessage())));
+                        format("Failed to retrieve data from %s Reason: %s", url, e.getMessage())));
             } catch (JSONException e) {
                 statuses.add(new Status(StatusCode.CRITICAL,
-                        String.format("Result returned from %s is not JSON. Reason: %s", url, e.getMessage())));
+                        format("Result returned from %s is not JSON. Reason: %s", url, e.getMessage())));
             }
             finally {
                 checkVersionDto.closeConnection();
@@ -82,18 +87,32 @@ public class CheckVersionService {
         try {
             checkVersionDto.saveDownloadList(forDownload);
         } catch (IOException e) {
-            statuses.add(new Status(StatusCode.CRITICAL, String.format("Failed to save download list. Reason: %s", e.getMessage())));
+            statuses.add(new Status(StatusCode.CRITICAL, format("Failed to save download list. Reason: %s", e.getMessage())));
         }
 
         return statuses;
     }
 
-    private boolean isDownloaded(JSONObject json, ApplicationList downloadedApps, String latestVersion) {
+    private boolean isInDownloadList(JSONObject json, ApplicationList applicationList) {
+        if (applicationList != null && applicationList.getApplications() != null) {
+            for (ApplicationData data : applicationList.getApplications()) {
+                if (data.getName().getArtifactId().equals(json.getString("groupId"))
+                    && data.getName().getGroupId().equals(json.getString("artifactId")))
+                    if (data.getActiveVersion() != null && data.getActiveVersion().equals(json.getString("version"))) {
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isDownloaded(JSONObject json, ApplicationList downloadedApps) {
         if (downloadedApps != null) {
             for (ApplicationData downloaded : downloadedApps.getApplications()) {
                 if (downloaded.getName().getGroupId().equals(json.getString("groupId"))
                         && downloaded.getName().getArtifactId().equals(json.getString("artifactId"))) {
-                    if (downloaded.getActiveVersion() == null || downloaded.getActiveVersion().equals(latestVersion)) {
+                    if (downloaded.getActiveVersion() == null
+                            || downloaded.getActiveVersion().equals(json.getString("version"))) {
                         return true;
                     }
                 }
