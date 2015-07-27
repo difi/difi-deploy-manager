18b41f8bd3c08f836d6e5789fb4e2b20d3535fc7
Stein-Bjarne <stein-bjarne.johansen@inmeta.com>
2015-07-27 15:59:08 +0200

    Logging of result when scheduler has run

--- src/main/java/no/difi/deploymanager/schedule/Scheduler.java
+++ src/main/java/no/difi/deploymanager/schedule/Scheduler.java
@@ -1,13 +1,19 @@
 package no.difi.deploymanager.schedule;
 
 import no.difi.deploymanager.domain.Status;
+import no.difi.deploymanager.domain.StatusCode;
 import no.difi.deploymanager.download.service.DownloadService;
+import no.difi.deploymanager.restart.service.RestartService;
+import no.difi.deploymanager.versioncheck.service.CheckVersionService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import no.difi.deploymanager.restart.service.RestartService;
-import no.difi.deploymanager.versioncheck.service.CheckVersionService;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -21,6 +27,8 @@ public class Scheduler {
     private final DownloadService downloadService;
     private final RestartService restartService;
 
+    private static final Logger logger = LogManager.getLogger(Scheduler.class);
+
     @Autowired
     public Scheduler(CheckVersionService checkVersionService, DownloadService downloadService, RestartService restartService) {
         this.checkVersionService = checkVersionService;
@@ -31,18 +39,51 @@ public class Scheduler {
 
     @Scheduled(cron = CRON_RUN_CHECK_FOR_VERSION)
     public void checkForNewVersion() {
-        List<Status> result = checkVersionService.execute();
-        //TODO: Result from checkVersionService needs to be logged.
+        LocalDateTime start = LocalDateTime.now();
+
+        logStatus(checkVersionService.execute());
+
+        LocalDateTime stop = LocalDateTime.now();
+        Duration timeUsed = Duration.between(start, stop);
+
+        logger.log(Level.INFO, String.format("Checking for new versions took %d sec to run.", timeUsed.getSeconds()));
     }
 
     @Scheduled(cron = CRON_RUN_DOWNLOAD_NEW_VERSION)
     public void downloadNewVersion() {
-        List<Status> result = downloadService.execute();
-        //TODO: Result from downloadService needs to be logged.
+        LocalDateTime start = LocalDateTime.now();
+
+        logStatus(downloadService.execute());
+
+        LocalDateTime stop = LocalDateTime.now();
+        Duration timeUsed = Duration.between(start, stop);
+
+        logger.log(Level.INFO, String.format("Checking for new versions took %d sec to run.", timeUsed.getSeconds()));
     }
 
     @Scheduled(cron = CRON_RESTART_APPLICATIONS)
     public void restartApplications() {
-        List<Status> result = restartService.execute();
+        LocalDateTime start = LocalDateTime.now();
+
+        logStatus(restartService.execute());
+
+        LocalDateTime stop = LocalDateTime.now();
+        Duration timeUsed = Duration.between(start, stop);
+
+        logger.log(Level.INFO, String.format("Checking for new versions took %d sec to run.", timeUsed.getSeconds()));
+    }
+
+    private void logStatus(List<Status> result) {
+        Level logLevel;
+        for (Status status : result) {
+            if (status.getStatusCode() == StatusCode.SUCCESS) {
+                logLevel = Level.INFO;
+            } else if (status.getStatusCode() == StatusCode.ERROR) {
+                logLevel = Level.ERROR;
+            } else {
+                logLevel = Level.FATAL;
+            }
+            logger.log(logLevel, status.getDescription());
+        }
     }
 }
