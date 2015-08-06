67fc6f1c780f6e4bc2051a672b3fc49dd66ef862
Stein-Bjarne <stein-bjarne.johansen@inmeta.com>
2015-08-06 10:16:41 +0200

    Adding more mock-data

--- difi-deploy-manager-admin/src/main/java/no/difi/deploymanager/dao/ArtifactDao.java
+++ difi-deploy-manager-admin/src/main/java/no/difi/deploymanager/dao/ArtifactDao.java
@@ -10,17 +10,25 @@ import java.util.List;
 @Repository
 public class ArtifactDao {
     public ArtifactList fetchArtifactList() {
-        Artifact app1 = new Artifact();
-        app1.setName("difi-deploy-manager");
-        app1.setGroupId("difi-deploy-manager");
-        app1.setArtifactId("no.difi.deploymanager");
-        app1.setVersion("0.9.1-SNAPSHOT");
-        app1.setApplicationType(ArtifactType.JAR);
+        Artifact art1 = new Artifact();
+        art1.setName("difi-deploy-manager");
+        art1.setGroupId("difi-deploy-manager");
+        art1.setArtifactId("no.difi.deploymanager");
+        art1.setVersion("0.9.1-SNAPSHOT");
+        art1.setApplicationType(ArtifactType.JAR);
+
+        Artifact art2 = new Artifact();
+        art2.setName("Deploy manager health check");
+        art2.setGroupId("difi-deploy-manager-health-check");
+        art2.setArtifactId("difi-deploy-manager-health-check");
+        art2.setVersion("0.9.1-SNAPSHOT");
+        art2.setApplicationType(ArtifactType.JAR);
 
         ArtifactList artifactList = new ArtifactList();
 
         List<Artifact> applications = artifactList.getArtifacts();
-        applications.add(app1);
+        applications.add(art1);
+        applications.add(art2);
 
         artifactList.setArtifacts(applications);
 
