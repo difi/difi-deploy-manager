2d7291cd501053360e5dc1892912cd4e274e4c02
Stein-Bjarne <stein-bjarne.johansen@inmeta.com>
2015-08-28 15:26:42 +0200

    Cleaning up test and unused files

--- deploy-manager/src/test/java/no/difi/deploymanager/restart/dao/RestartCommandLineIntegrationTest.java
+++ deploy-manager/src/test/java/no/difi/deploymanager/restart/dao/RestartCommandLineIntegrationTest.java
@@ -1,8 +1,11 @@
 package no.difi.deploymanager.restart.dao;
 
 import no.difi.deploymanager.artifact.Application;
+import no.difi.deploymanager.domain.ApplicationData;
+import no.difi.deploymanager.domain.Self;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -22,7 +25,7 @@ public class RestartCommandLineIntegrationTest {
     @SuppressWarnings("SpringJavaAutowiredMembersInspection")
     @Autowired Environment environment;
 
-    private static final String TEST_APPLICATION_FILENAME = "deploy-manager-health-check-0.9.0.jar";
+    private static final String TEST_APPLICATION_FILENAME = "deploy-manager-health-check-0.9.25.jar";
     private static final String TEMP_TEST_JAR_FILE = "./bin/" + TEST_APPLICATION_FILENAME;
     private static final String PERM_TEST_JAR_FILE = "./bin-test/" + TEST_APPLICATION_FILENAME;
     private static final String TEMP_TEST_JAR_FILE_WIN = System.getProperty("user.dir") + "\\bin\\" + TEST_APPLICATION_FILENAME;
@@ -41,6 +44,37 @@ public class RestartCommandLineIntegrationTest {
         }
 
         restartCommandLine = new RestartCommandLine(environment);
+    }
+
+    @Test
+    public void should_start_and_restart_and_stop_running_process_on_current_OS() throws Exception {
+        if (!IS_WINDOWS) {
+            Runtime.getRuntime().exec(new String[]{"cp", PERM_TEST_JAR_FILE, TEMP_TEST_JAR_FILE});
+        }
+        else {
+            Runtime.getRuntime().exec("cmd /c copy " + PERM_TEST_JAR_FILE_WIN + " "  + TEMP_TEST_JAR_FILE_WIN);
+        }
+
+        ApplicationData oldApp = new ApplicationData.Builder()
+                .name("no.difi.deploymanager")
+                .filename(TEST_APPLICATION_FILENAME)
+                .activeVersion("0.9.1")
+                .build();
+
+        ApplicationData newApp = new ApplicationData.Builder()
+                .name("no.difi.deploymanager")
+                .filename(TEST_APPLICATION_FILENAME)
+                .activeVersion("0.9.2")
+                .build();
+
+        Self self = new Self.Builder()
+                .name("no.no.no")
+                .version("0.9.3")
+                .build();
+
+//        assertTrue(restartCommandLine.startProcess(newApp));
+//        assertTrue(restartCommandLine.executeRestart(oldApp, newApp, self));
+//        assertTrue(restartCommandLine.stopProcess(newApp));
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
