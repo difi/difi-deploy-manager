e33390d971c94b6ce8e086210b66fd2448b03f08
Stein-Bjarne <stein-bjarne.johansen@inmeta.com>
2015-08-28 15:51:06 +0200

    Testing command line integration test

--- deploy-manager/src/test/java/no/difi/deploymanager/restart/dao/RestartCommandLineIntegrationTest.java
+++ deploy-manager/src/test/java/no/difi/deploymanager/restart/dao/RestartCommandLineIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.File;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -72,9 +73,9 @@ public class RestartCommandLineIntegrationTest {
                 .version("0.9.3")
                 .build();
 
-//        assertTrue(restartCommandLine.startProcess(newApp));
-//        assertTrue(restartCommandLine.executeRestart(oldApp, newApp, self));
-//        assertTrue(restartCommandLine.stopProcess(newApp));
+        assertTrue(restartCommandLine.startProcess(newApp));
+        assertTrue(restartCommandLine.executeRestart(oldApp, newApp, self));
+        assertTrue(restartCommandLine.stopProcess(newApp));
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
